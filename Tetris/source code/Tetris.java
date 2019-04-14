968c838a0b78d3734f8b8d62a4fa6db4dccf1fa8
Colby Lewis <ColbyK.Lewis@gmail.com>
2019-04-14 00:01:39 -0500

    Update Tetris.java

--- Tetris/source code/Tetris.java
+++ Tetris/source code/Tetris.java
@@ -6,6 +6,7 @@
  *
  */
 import java.awt.Color;
+import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.Graphics;
 
@@ -16,9 +17,20 @@ import javax.swing.JPanel;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
-public class Tetris extends JPanel {
+import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JTextArea;
+import javax.swing.JTextPane;
+import java.awt.Label;
+import java.awt.TextField;
+import java.awt.Canvas;
+import java.awt.Panel;
+public class Tetris extends JPanel{
 
 	private Game game;
+	public Grid grid;
 
 	/**
 	 * Sets up the parts for the Tetris game, display and user control
@@ -26,13 +38,19 @@ public class Tetris extends JPanel {
 	public Tetris() {
 		game = new Game(this);
 		JFrame f = new JFrame("The Tetris Game");
-		f.add(this);
+		f.getContentPane().add(this);
 		f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		f.setSize(450, 555);
 		f.setVisible(true);
 		EventController ec = new EventController(game);
 		f.addKeyListener(ec);
-		setBackground(Color.yellow);
+		setBackground(Color.PINK);
+		setLayout(null);
+		
+		JLabel lblScore = new JLabel("SCORE:");
+		lblScore.setBounds(12, 13, 61, 22);
+		lblScore.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		add(lblScore);
 		
 	}
 
@@ -54,11 +72,22 @@ public class Tetris extends JPanel {
 			g.setColor(Color.BLACK.darker());
 			g.drawString("GAME OVER", 80, 300);
 		}
+		g.setColor(Color.BLACK);
+		g.setFont(new Font("Palatino",Font.PLAIN, 20));
+		g.drawString("" + grid.score, 80, 30 );
 	}
 	
 	public static void main(String[] args) {
-		new Tetris();
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Tetris frame = new Tetris();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 		
 	}
-
 }
